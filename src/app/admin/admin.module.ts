1df9e7dd33587c8c4890cfdadd2ebca09d9dcb00
Tarasikee <51419482+Tarasikee@users.noreply.github.com>
2020-12-15 22:30:36 +0200

    Minor adding functionality 0.04v

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -6,11 +6,21 @@ import {LoginPageComponent} from './login-page/login-page.component';
 import {DashboardComponent} from './dashboard/dashboard.component';
 import {CreatePageComponent} from './create-page/create-page.component';
 import {EditPageComponent} from './edit-page/edit-page.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {AuthService} from './shared/services/auth.service';
 
 @NgModule({
-  declarations: [AdminLayoteComponent, LoginPageComponent, DashboardComponent, CreatePageComponent, EditPageComponent],
+  declarations: [
+    AdminLayoteComponent,
+    LoginPageComponent,
+    DashboardComponent,
+    CreatePageComponent,
+    EditPageComponent
+  ],
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     RouterModule.forChild([
       {
         path: '', component: AdminLayoteComponent, children: [
@@ -25,6 +35,9 @@ import {EditPageComponent} from './edit-page/edit-page.component';
   ],
   exports: [
     RouterModule
+  ],
+  providers: [
+    AuthService
   ]
 })
 export class AdminModule {
--- src/app/admin/shared/components/admin-layote/admin-layote.component.ts
+++ src/app/admin/shared/components/admin-layote/admin-layote.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-admin-layote',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminLayoteComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) {
+  }
 
   ngOnInit(): void {
   }
 
+  logout(e: Event): void {
+    e.preventDefault();
+    this.router.navigate(['/admin']);
+  }
 }
