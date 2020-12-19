dd579b68d4a5a4f198aa7d8bd92352859402ec6b
Tarasikee <51419482+Tarasikee@users.noreply.github.com>
2020-12-19 22:35:47 +0200

    New server workers and interceptors v0.0851

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -27,6 +27,8 @@ export class LoginPageComponent implements OnInit {
     this.route.queryParams.subscribe((params: Params) => {
       if (params.loginAgain) {
         this.message = 'Please, login again';
+      } else if (params.authFailed) {
+        this.message = 'Session is expired. Login again!';
       }
     });
 
