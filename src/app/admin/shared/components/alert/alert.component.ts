ba25e929fdbfa22669a42eef26a83bed427f5eb5
Tarasikee <51419482+Tarasikee@users.noreply.github.com>
2020-12-20 20:58:20 +0200

    New alert type messages for admin v0.098

--- src/app/admin/shared/components/alert/alert.component.ts
+++ src/app/admin/shared/components/alert/alert.component.ts
@@ -1,20 +1,37 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
+import {AlertService} from '../../services/alert.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-alert',
   templateUrl: './alert.component.html',
   styleUrls: ['./alert.component.sass']
 })
-export class AlertComponent implements OnInit {
+export class AlertComponent implements OnInit, OnDestroy {
 
   @Input() delay = 5000;
   public text!: string;
   public type!: string;
+  aSub!: Subscription;
 
-  constructor() {
+  constructor(
+    private alertService: AlertService
+  ) {
   }
 
   ngOnInit(): void {
+    this.alertService.alert$.subscribe(alert => {
+      this.type = alert.type;
+      this.text = alert.text;
+
+      const timeout = setTimeout(() => {
+        clearTimeout(timeout);
+        this.text = '';
+      }, this.delay);
+    });
   }
 
+  ngOnDestroy(): void {
+
+  }
 }
--- src/app/admin/shared/services/alert.service.ts
+++ src/app/admin/shared/services/alert.service.ts
@@ -1,7 +1,27 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {Subject} from 'rxjs';
+
+export type AlertType = 'success' | 'warning' | 'danger';
+
+export interface IAlert {
+  type: AlertType;
+  text: string;
+}
 
 @Injectable()
 export class AlertService {
+  public alert$ = new Subject<IAlert>();
 
-  constructor() { }
+  success(text: string): void {
+    this.alert$.next({type: 'success', text});
+  }
+
+  warning(text: string): void {
+    this.alert$.next({type: 'warning', text});
+  }
+
+  danger(text: string): void {
+    this.alert$.next({type: 'danger', text});
+  }
+
 }
